b1e32a0fc5a660e385e09427e3ae07f2ee619241
Maggie McCausland <maggiemccausland@hotmail.com>
2023-09-23 11:23:00 -0600

    changed to 24 hour time for elements and variables, started click event for hours

--- assets/script.js
+++ assets/script.js
@@ -9,22 +9,22 @@ var hour9El = $("#hour-9");
 var hour10El = $("#hour-10");
 var hour11El = $("#hour-11");
 var hour12El = $("#hour-12");
-var hour1El = $("#hour-1");
-var hour2El = $("#hour-2");
-var hour3El = $("#hour-3");
-var hour4El = $("#hour-4");
-var hour5El = $("#hour-5");
+var hour13El = $("#hour-13");
+var hour14El = $("#hour-14");
+var hour15El = $("#hour-15");
+var hour16El = $("#hour-16");
+var hour17El = $("#hour-17");
 
 // CREATE ARRAYS FOR HOUR LOCAL STORAGE
 var hour9 = [];
 var hour10 = [];
 var hour11 = [];
 var hour12 = [];
-var hour1 = [];
-var hour2 = [];
-var hour3 = [];
-var hour4 = [];
-var hour5 = [];
+var hour13 = [];
+var hour14 = [];
+var hour15 = [];
+var hour16 = [];
+var hour17 = [];
 
 $(function planner() {
   // COMPLETE: Display the current date in the header of the page.
@@ -38,11 +38,11 @@ $(function planner() {
   // current hour in 24-hour time?
   $(function currentTime() {
     var currentTime = dayjs().hour();
-    console.log("currentTime:", currentTime);
+    console.log("current Time:", currentTime);
 
     // Loop through each element with a class of time-block and compare their data-value 
     // with the current time to check for match.
-    $(".time-block").each(function () {
+    $(".time-block").each(function colorTime() {
       let timeDiv = $(this).attr('data-value');
 
       if (currentTime == timeDiv) {
@@ -56,7 +56,6 @@ $(function planner() {
     })
   });
 
-
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -64,6 +63,12 @@ $(function planner() {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
+  $('.time-block').on('click', function(e) {
+    var selectedHour = $(this);
+    $(selectedHour).addClass('testing');
+    e.preventDefault();
+
+});
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea Elements. HINT: How can the id
