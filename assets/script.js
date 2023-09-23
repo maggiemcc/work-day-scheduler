39434d74f7808d787dae15210c2413c8ce057e76
Maggie McCausland <maggiemccausland@hotmail.com>
2023-09-23 09:39:34 -0600

    created variables for HTML hour divs and hour arrays for storage

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,30 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
+// the code isn't run until the browser has finished rendering all the Elements
 // in the html.
+
+// VARIABLES FOR GETTING HOUR DIVS
+var hour9El = $("#hour-9");
+var hour10El = $("#hour-10");
+var hour11El = $("#hour-11");
+var hour12El = $("#hour-12");
+var hour1El = $("#hour-1");
+var hour2El = $("#hour-2");
+var hour3El = $("#hour-3");
+var hour4El = $("#hour-4");
+var hour5El = $("#hour-5");
+
+// CREATE ARRAYS FOR HOUR LOCAL STORAGE
+var hour9 = [];
+var hour10 = [];
+var hour11 = [];
+var hour12 = [];
+var hour1 = [];
+var hour2 = [];
+var hour3 = [];
+var hour4 = [];
+var hour5 = [];
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -16,7 +40,7 @@ $(function () {
   // current hour in 24-hour time?
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
+  // the values of the corresponding textarea Elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
