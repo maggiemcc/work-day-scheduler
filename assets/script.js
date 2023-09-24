3bd3d3e8b8db7e46385a871ad78302efcea37e5f
Maggie McCausland <maggiemccausland@hotmail.com>
2023-09-24 10:56:32 -0600

    deleted variables not in use, created displaySchedule() to display stored items in textareas

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,7 @@
 $(function () {
-
-  // VARIABLES FOR GETTING HOUR DIVS
-  // NEED THESE VARS?
-  // var hour9El = $("#hour-9");
-  // var hour10El = $("#hour-10");
-  // var hour11El = $("#hour-11");
-  // var hour12El = $("#hour-12");
-  // var hour13El = $("#hour-13");
-  // var hour14El = $("#hour-14");
-  // var hour15El = $("#hour-15");
-  // var hour16El = $("#hour-16");
-  // var hour17El = $("#hour-17");
-
-
-  var theDate = $("#currentDay");
-
   // DISPLAY CURRENT DATE IN HEADER
   function displayCurrentDate() {
+    var theDate = $("#currentDay");
     var currentDay = dayjs().format("dddd: MMMM DD, YYYY");
     theDate.text(currentDay);
   }
@@ -25,9 +10,11 @@ $(function () {
   function getCurrentTime() {
     var currentTime = dayjs().hour();
 
+    // ITERATE OVER .time-blocks USING JQUERY EACH
     $(".time-block").each(function () {
       var timeDiv = $(this).attr("data-value");
 
+      // CHECK FOR MACHING TIME
       if (currentTime == timeDiv) {
         $(this).addClass("present");
       } else if (currentTime < timeDiv) {
@@ -38,21 +25,33 @@ $(function () {
     });
   };
 
-
-  // SAVE BUTTON - SAVE USER INPUT
+  // SAVE BUTTON CLICK EVENT - SAVE USER INPUT
   $(".saveBtn").on("click", function (event) {
     event.preventDefault();
 
     // GET THE USER INPUT AND HOUR OF THE CHOSEN SAVE BUTTON USING THIS & JQUERY SIBLINGS
-    var userPlans = $(this).siblings(".description").val();
+    var userPlans = $(this).siblings(".description").val().trim();
     var selectedSaveTime = $(this).siblings(".hour").html();
 
     // SET KEY TO SELECTED HOUR AND VALUE TO USER INPUT FOR CORRESPONDING HOUR
     localStorage.setItem(selectedSaveTime, userPlans);
   });
 
+  // DISPLAY STORED DATA
+  function displaySchedule() {
+    // ITERATE OVER EACH .time-block USING JQUERY EACH TO GET STORED DATA
+    $(".hour").each(function () {
+      var savedTime = $(this).html();
+      var savedPlans = localStorage.getItem(savedTime)
+
+      // DISPLAY STORED DATA IN APPROPRIATE TEXTAREA
+      $(this).siblings(".description").val(savedPlans)
+    });
+  };
+
 
   // CALL FUNCTIONS
   displayCurrentDate();
   getCurrentTime();
+  displaySchedule()
 });
